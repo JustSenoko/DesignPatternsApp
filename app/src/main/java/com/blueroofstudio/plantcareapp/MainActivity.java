8eedd1b06db6192c72034ddebe8c9363775e8507
JustSenoko <just.senoko@gmail.com>
2020-01-13 22:50:35 +0300

    plant builder

--- app/src/main/java/com/blueroofstudio/plantcareapp/MainActivity.java
+++ app/src/main/java/com/blueroofstudio/plantcareapp/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.blueroofstudio.plantcareapp.models.Plant;
+import com.blueroofstudio.plantcareapp.models.PlantBuilder;
+
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Plant plant = new PlantBuilder(1, "Cactus")
+                .addWatering(new Date(2020, 1, 1), 10)
+                .addTransplanting(new Date(2020, 1, 1), 12)
+                .build();
     }
 }
